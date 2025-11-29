e14bd2ea39ba965a6888019d678e27a7238a943f
livekiron <livebangla2021@gmail.com>
2025-11-29 22:53:24 +0600

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,26 +1,12 @@
+let isVerified = false;
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.action === "verifyEmail") {
-        const email = encodeURIComponent(msg.email);
-        const device = crypto.randomUUID();
+  if (msg.action === "setVerified") {
+    isVerified = true;
+    sendResponse({ success: true });
+  }
 
-        const url = `https://kiron-extension-auth.vercel.app/api/auth/verify?email=${email}&device=${device}`;
-
-        fetch(url, {
-            method: "GET"
-        })
-        .then(res => res.json())
-        .then(data => {
-            if (data.success) {
-                chrome.storage.local.set({ verified: true });
-                sendResponse({ success: true });
-            } else {
-                sendResponse({ success: false, error: data.error || "Server error" });
-            }
-        })
-        .catch(err => {
-            sendResponse({ success: false, error: "Network error / CORS error" });
-        });
-
-        return true; // async response allowed
-    }
+  if (msg.action === "isVerified") {
+    sendResponse({ verified: isVerified });
+  }
 });
