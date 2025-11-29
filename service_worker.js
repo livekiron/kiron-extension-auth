7e08a452e74f90d5c176e784fe7908b91430de9f
livekiron <livebangla2021@gmail.com>
2025-11-29 22:19:13 +0600

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -5,7 +5,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         target: { tabId },
         files: ["content_script.js"]
       })
-	  .catch(e => console.warn("Inject failed:", e));
+	  
     }
   }
 });
+
